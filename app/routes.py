5d0991bb06bf0a355cde397a097fed0fce392429
jungyin <noel@beyondplatform.co.kr>
2021-12-08 12:48:09 +0900

    -

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'jamie'}
-    return render_template('index.html', title='home', user=user)
+    posts = [
+        {
+            'author': {'username': 'jamie1'},
+            'body': 'test1'
+        },
+        {
+            'author': {'username': 'jamie2'},
+            'body': 'test2'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
